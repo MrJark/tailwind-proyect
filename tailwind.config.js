7065601f9c8da198abfb144c0e6e5e3122520661
MrJark <mrjark7@gmail.com>
2023-02-26 07:49:50 +0100

    've modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  purge: [],
+  darkMode: false,
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
+    extend: {
+      
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
