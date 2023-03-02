c8fc874913d2d6d0e22aafed4523c88cc3556427
MrJark <mrjark7@gmail.com>
2023-03-02 07:25:10 +0100

    've managed to get it to work becausa i'd to change the file tailwind.config.js and package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  purge: [],
-  darkMode: false,
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],  
   theme: {
+    fontFamily: {
+      K2D: ['K2D', 'sans-serif'],
+    },
+    textColor: {
+      'primary': '#09AEB3',
+      'secondary': '#FF01B0',
+      'terciary': '#FFDC19',
+      'black': '#0B0A0B',
+      'white': '#F9F9F9',
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#09AEB3',
+      'secondary': '#FF01B0',
+      'terciary': '#FFDC19',
+      'black': '#0B0A0B',
+      'white': '#F9F9F9',
+      'background-dark': '#191D1D',
+      'background-light': '#F3FFFF',
+    }),
     extend: {
-      backgroundImage: {
         'madrid': "url('/public/img/madrid.jpeg')",
         'barcelona': "url('/public/img/barcelona.jpeg')",
         'galicia': "url('/public/img/galicia.jpeg')",
         'alicante': "url('/public/img/alicante.jpeg')",
         'murcia': "url('/public/img/murcia.jpeg')",
         'sevilla': "url('/public/img/sevilla.jpeg')",
-        'valencia': "url('/public/img/valencia.jpeg')",
-      },
-      backgroundColor: theme => ({
-        ...theme('colors'),
-        'primary': '#09AEB3',
-        'secondary': '#FF01B0',
-        'terciary': '#FFDC19',
-        'black': '#0B0A0B',
-        'white': '#F9F9F9',
-        'background-dark': '#191D1D',
-        'background-light': '#F3FFFF',
-      }),
-      textColor: {
-        'primary': '#09AEB3',
-        'secondary': '#FF01B0',
-        'terciary': '#FFDC19',
-        'black': '#0B0A0B',
-        'white': '#F9F9F9',
-      }
+        'valencia': "url('/public/img/valencia.jpeg')",    
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [
+  ],
 };
