c0d976d1d08e0b8985bc198ba9a996ddc566c3fa
MrJark <mrjark7@gmail.com>
2023-03-02 06:37:49 +0100

    've added the prototype build i figma and logo assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
-      
+      backgroundImage: {
+        'madrid': "url('/public/img/madrid.jpeg')",
+        'barcelona': "url('/public/img/barcelona.jpeg')",
+        'galicia': "url('/public/img/galicia.jpeg')",
+        'alicante': "url('/public/img/alicante.jpeg')",
+        'murcia': "url('/public/img/murcia.jpeg')",
+        'sevilla': "url('/public/img/sevilla.jpeg')",
+        'valencia': "url('/public/img/valencia.jpeg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary'; '#09AEB3',
+        'secondary'; '#FF01B0',
+        'terciary'; '#FFDC19',
+        'black'; '#0B0A0B',
+        'white'; '#F9F9F9',
+        'background-dark'; '#191D1D',
+        'background-light'; '#F3FFFF',
+      })
     },
   },
   variants: {
